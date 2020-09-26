ea5d2568273dc5f9353c494b657d301f43e224ae
grishberg <grishberg@ya.ru>
2020-09-26 15:40:31 +0300

    initial commit

---
+++ src/main/kotlin/com/github/grishberg/android/profiler/core/AnalyzerResult.kt
@@ -0,0 +1,10 @@
+package com.github.grishberg.android.profiler.core
+
+interface AnalyzerResult {
+    val threadTimeBounds: Map<Int, ThreadTimeBounds>
+    val globalTimeBounds: Map<Int, ThreadTimeBounds>
+    val maxLevel: Int
+    val data: Map<Int, List<ProfileData>>
+    val threads: List<ThreadItem>
+    val mainThreadId: Int
+}
---
+++ src/main/kotlin/com/github/grishberg/android/profiler/core/ThreadItem.kt
@@ -0,0 +1,6 @@
+package com.github.grishberg.android.profiler.core
+
+interface ThreadItem {
+    val name: String
+    val threadId: Int
+}
